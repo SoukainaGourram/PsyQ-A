83128e8ff2fac18a7232aa174e0ebf28a9468eaa
soukaina <soukaina@gr.com>
2024-03-31 21:24:37 +0000

    update

--- PROJECT/script.js
+++ PROJECT/script.js
@@ -1,12 +1,9 @@
-
 const questions = document.querySelectorAll(".question");
 
 questions.forEach(function (question) {
   const btn = question.querySelector(".question-btn");
-  
-  btn.addEventListener("click", function () {
-    
 
+  btn.addEventListener("click", function () {
     questions.forEach(function (item) {
       if (item !== question) {
         item.classList.remove("show-text");
